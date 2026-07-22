a4bd12e4e202e9522e3516a7195fe20da5617261
amari-stack <amari@gigsta.ai>
2026-07-22 13:37:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/CivilContractors/",
 
   plugins: [react(), tailwindcss()],
 
